be40fdbf89f9b41cb24f5567bec465f6d894bbad
Nico Welles <nico@welles.email>
2025-12-16 20:32:54 +0100

    Auto optimise store on rebuild of configuration

--- configuration.nix
+++ configuration.nix
@@ -108,6 +108,9 @@
     vscode
   ];
 
+  # Auto optimise package store.
+  nix.settings.auto-optimise-store = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
