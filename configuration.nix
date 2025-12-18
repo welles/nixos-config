4575845698c098d28b30f75e2217b6e9ef1f42db
Nico Welles <nico@welles.email>
2025-12-18 20:45:49 +0100

    Clean up and format configuration file

--- configuration.nix
+++ configuration.nix
@@ -1,15 +1,8 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
 {
-  # imports =
-  #   [ # Include the results of the hardware scan.
-  #     ./hardware-configuration.nix
-  #   ];
-
+  config,
+  pkgs,
+  ...
+}: {
   boot.loader = {
     systemd-boot.enable = false;
     efi.canTouchEfiVariables = true;
@@ -21,44 +14,17 @@
       default = "saved";
       gfxmodeEfi = "1920x1080";
       gfxpayloadEfi = "keep";
-      # theme = "${pkgs.distro-grub-themes}/share/grub/themes/nixos";
-      # theme = pkgs.stdenv.mkDerivation {
-      #   pname = "distro-grub-themes";
-      #   version = "3.1";
-      #   src = pkgs.fetchFromGitHub {
-      #     owner = "AdisonCavani";
-      #     repo = "distro-grub-themes";
-      #     rev = "v3.1";
-      #     sha256 = "ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
-      #   };
-      #   installPhase = "cp -r customize/nixos $out";
-      # };
     };
   };
   catppuccin.grub = {
     enable = true;
-    flavor = "mocha"; # Auswahl: latte, frappe, macchiato, mocha
+    flavor = "mocha";
   };
 
-  # networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Add aliases for common commands.
-  # environment.shellAliases = {
-  #   rebuild = "sudo nixos-rebuild switch --flake ~/nixos#nixos";
-  # };
-
-  # Set your time zone.
   time.timeZone = "Europe/Berlin";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "de_DE.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -73,11 +39,8 @@
     LC_TIME = "de_DE.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm = {
     enable = true;
     settings = {
@@ -90,19 +53,15 @@
 
   services.power-profiles-daemon.enable = true;
 
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "de";
     variant = "";
   };
 
-  # Configure console keymap
   console.keyMap = "de";
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -110,27 +69,15 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.nico = {
     isNormalUser = true;
     initialPassword = "passwort";
     description = "Nico";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = ["networkmanager" "wheel"];
     shell = pkgs.zsh;
     packages = with pkgs; [
-    #  kdePackages.kate
-    #  thunderbird
     ];
   };
 
@@ -140,10 +87,8 @@
     boot.loader.timeout = 30;
   };
 
-  # Install firefox.
   programs.firefox.enable = true;
 
-  # Use nh for OS building.
   programs.nh = {
     enable = true;
     clean = {
@@ -161,14 +106,9 @@
     dedicatedServer.openFirewall = true;
   };
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
     lazygit
     vscode
     vesktop
@@ -180,10 +120,8 @@
     alejandra
   ];
 
-  # Auto optimise package store.
   nix.settings.auto-optimise-store = true;
 
-  # Enable NixOS flakes feature.
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
@@ -195,38 +133,5 @@
     cp ${./floating.png} /var/lib/AccountsService/icons/nico
   '';
 
-  # Remove all NixOS generations older than a week automatically.
-  # nix.gc = {
-  #   automatic = true;
-  #   dates = "weekly";
-  #   options = "--delete-older-than 7d";
-  # };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
-
 }
