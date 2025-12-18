0cbe52333c3bd45593b2a0892cbd87fb32b2815c
Nico Welles <nico@welles.email>
2025-12-18 12:19:45 +0100

    Change formatting of flake modules list

--- flake.nix
+++ flake.nix
@@ -6,7 +6,9 @@
   outputs = { self, nixpkgs }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+      modules = [
+        ./configuration.nix
+      ];
     };
   };
 }
