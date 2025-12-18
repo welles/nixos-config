2c52f4ccd36bf91f238ec11c58c5ff0cecc8ed34
Nico Welles <nico@welles.email>
2025-12-18 12:20:51 +0100

    Install home-manager module

--- flake.nix
+++ flake.nix
@@ -1,13 +1,25 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
+
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;          
+          home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.users.nico = import ./home.nix;
+        }
       ];
     };
   };
