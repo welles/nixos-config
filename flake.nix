08e674a371adc0fa644578fdd007d79a72e6e261
Nico Welles <nico@welles.email>
2025-12-18 15:38:43 +0100

    Fix source of plasma-manager module

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     };
     catppuccin.url = "github:catppuccin/nix";
     plasma-manager = {
-      url = "github:pjones/plasma-manager";
+      url = "github:nix-community/plasma-manager";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
