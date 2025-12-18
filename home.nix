554d19c7505a003e0887aa019112d189e3720c93
Nico Welles <nico@welles.email>
2025-12-18 20:46:21 +0100

    Clean up and format home file

--- home.nix
+++ home.nix
@@ -1,8 +1,8 @@
-{ config, pkgs, ... }:
-
 {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
+  config,
+  pkgs,
+  ...
+}: {
   home.username = "nico";
   home.homeDirectory = "/home/nico";
   home.file.".face.icon".source = ./floating.png;
@@ -63,15 +63,6 @@
       enable = true;
     };
   };
-
-  # programs.kitty = {
-  #   enable = true;
-  #   themeFile = "OneHalfDark";
-  #   settings = {
-  #     font_size = 12;
-  #     confirm_os_window_close = 0;
-  #   };
-  # };
 
   programs.plasma = {
     enable = true;
@@ -138,16 +129,7 @@
     SSH_AUTH_SOCK = "${config.home.homeDirectory}/.bitwarden-ssh-agent.sock";
   };
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "25.11";
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
