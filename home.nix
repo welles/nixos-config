15b2b8ed1485051176a4c0b9bbb9693aff28d790
Nico Welles <nico@welles.email>
2025-12-22 01:19:35 +0100

    Install starship

--- home.nix
+++ home.nix
@@ -102,6 +102,17 @@
     };
   };
 
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      character = {
+        success_symbol = "[➜](bold green)";
+        error_symbol = "[➜](bold red)";
+      };
+    };
+  };
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
