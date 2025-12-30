a158ce94623591ab2aa97fa279a2953b9d6adcb3
Nico Welles <nico@welles.email>
2025-12-30 16:33:58 +0100

    Add NVIDIA GPU support for ThinkBook

--- machines/nico-thinkbook-nixos/machine-configuration.nix
+++ machines/nico-thinkbook-nixos/machine-configuration.nix
@@ -5,4 +5,33 @@
   modulesPath,
   ...
 }: {
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    modesetting.enable = true;
+
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+
+    open = true;
+
+    nvidiaSettings = false;
+
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:2:0:0";
+    };
+  };
 }
