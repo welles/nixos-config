027427e601975f13e9b4e0f78920ce28eef9d458
Nico Welles <nico@welles.email>
2026-01-04 18:38:45 +0100

    Set flickr as background of the day provider

--- home.nix
+++ home.nix
@@ -410,7 +410,7 @@
             var d = allDesktops[i];
             d.wallpaperPlugin = "org.kde.potd";
             d.currentConfigGroup = ["Wallpaper", "org.kde.potd", "General"];
-            //d.writeConfig("Provider", "wcpotd"); // wcpotd = Wikimedia Commons
+            d.writeConfig("Provider", "flickr");
             d.writeConfig("FillMode", "2");
             d.writeConfig("UpdateOverMeteredConnection", "1");
             d.reloadConfig();
