e7e586c789008acf3f54b73c8bb614b53f082df4
Nico Welles <nico@welles.email>
2026-01-15 23:34:55 +0100

    Add swap partition to parents laptop disk config

--- machines/eltern-asus-nixos/disk-configuration.nix
+++ machines/eltern-asus-nixos/disk-configuration.nix
@@ -14,7 +14,16 @@
               end = "1G";
               type = "EF02";
             };
+            swap = {
+              priority = 2;
+              size = "8G";
+              content = {
+                type = "swap";
+                resumeDevice = true;
+              };
+            };
             root = {
+              priority = 3;
               size = "100%";
               content = {
                 type = "filesystem";
