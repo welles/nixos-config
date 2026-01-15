b905eebab4b0d8ed7c903d22dbce7f20626deb81
Nico Welles <nico@welles.email>
2026-01-15 22:36:08 +0100

    Add machine and disk configuration for parents laptop

---
+++ machines/eltern-asus-nixos/disk-configuration.nix
@@ -0,0 +1,30 @@
+{
+  disko.devices = {
+    disk = {
+      vda = {
+        type = "disk";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              priority = 1;
+              name = "boot";
+              start = "0";
+              end = "1G";
+              type = "EF02";
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
