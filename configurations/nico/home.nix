3b770f73e10fac3405908469a8e9298b5279abf2
Nico Welles <nico@welles.email>
2026-01-16 14:30:53 +0100

    Use main as default branch name in git

--- configurations/nico/home.nix
+++ configurations/nico/home.nix
@@ -28,6 +28,7 @@
       user.name = "Nico Welles";
       user.email = "nico@welles.email";
       gpg.format = "ssh";
+      init.defaultBranch = "main";
     };
     signing = {
       key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOKIfp1vknvLG8NUOIq6BAh8rIAq96kU+bbem0HtopQL";
