4c9ae8e651b4ca299cb55f6ebb9e7a2c9ebcdb4f
Nico Welles <nico@welles.email>
2026-01-16 01:57:44 +0100

    Make boot quieter and show logo on parents laptop

--- configurations/eltern/configuration.nix
+++ configurations/eltern/configuration.nix
@@ -10,6 +10,24 @@
         device = "nodev";
       };
     };
+    kernelParams = [
+      "splash"
+      "quiet"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+    initrd.verbose = false;
+    consoleLogLevel = 0;
+    plymouth = {
+      enable = true;
+      theme = "rings";
+      themePackages = with pkgs; [
+        (adi1090x-plymouth-themes.override {
+          selected_themes = ["rings"];
+        })
+      ];
+    };
   };
 
   services = {
