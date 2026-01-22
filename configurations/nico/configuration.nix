2d27df04ccc93cc99e15add5d488215694455e21
Nico Welles <nico@welles.email>
2026-01-22 10:29:33 +0100

    Add PolicyKit and rule that allows mounting of disks from KDE disk GUI

--- configurations/nico/configuration.nix
+++ configurations/nico/configuration.nix
@@ -60,6 +60,19 @@
 
   services.gvfs.enable = true;
 
+  security.polkit = {
+    enable = true;
+    extraConfig = ''
+      polkit.addRule(function(action, subject) {
+        if ((action.id == "org.freedesktop.udisks2.filesystem-mount-system" ||
+             action.id == "org.freedesktop.udisks2.filesystem-mount") &&
+            subject.isInGroup("wheel")) {
+          return polkit.Result.YES;
+        }
+      });
+    '';
+  };
+
   hardware = {
     # xpadneo.enable = true;
     xone.enable = true;
