787b728f195330dbfc57a5baf64f48e532efe2a9
Nico Welles <nico@welles.email>
2026-01-27 16:16:20 +0100

    Enable LTE networking for ThinkPad

--- machines/nico-thinkpad-nixos/machine-configuration.nix
+++ machines/nico-thinkpad-nixos/machine-configuration.nix
@@ -11,7 +11,30 @@
     libinput
     iio-sensor-proxy
     maliit-keyboard
+    mobile-broadband-provider-info
   ];
 
   services.fprintd.enable = true;
+
+  networking.modemmanager.enable = true;
+
+  networking.networkmanager.ensureProfiles.profiles = {
+    "Telekom LTE" = {
+      connection = {
+        id = "Telekom LTE";
+        uuid = "1ff5123a-440e-4e1b-80d2-ee47cd559c84";
+        type = "gsm";
+      };
+      gsm = {
+        apn = "internet.telekom";
+      };
+      ipv4 = {
+        method = "auto";
+      };
+      ipv6 = {
+        addr-gen-mode = "default";
+        method = "auto";
+      };
+    };
+  };
 }
