9ab8e7d4b31418179626febb06b875937fc8f59a
Nico Welles <nico@welles.email>
2026-02-10 17:39:11 +0100

    Add zsh and fastfetch to vm

--- configurations/vm/home.nix
+++ configurations/vm/home.nix
@@ -2,6 +2,23 @@
   home.username = "vm";
   home.homeDirectory = "/home/vm";
 
+  programs.fastfetch = {
+    enable = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    initContent = "fastfetch";
+
+    oh-my-zsh = {
+      enable = true;
+    };
+  };
+
   home.stateVersion = "25.11";
 
   programs.home-manager.enable = true;
