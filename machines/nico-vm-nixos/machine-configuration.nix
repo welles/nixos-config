4f9ba2c8317c8a7212c854dabae6928cd71a0103
Nico Welles <nico@welles.email>
2026-02-14 13:50:23 +0100

    Generate NFS shares dynamically from array

--- machines/nico-vm-nixos/machine-configuration.nix
+++ machines/nico-vm-nixos/machine-configuration.nix
@@ -4,18 +4,14 @@
   pkgs,
   modulesPath,
   ...
-}: {
-  fileSystems."/mnt/user/emulation" = {
-    device = "10.0.0.10:/mnt/user/emulation";
+}: let
+  nfsShares = ["emulation" "temp"];
+in {
+  fileSystems = lib.genAttrs (map (share: "/mnt/user/${share}") nfsShares) (mountPoint: {
+    device = "10.0.0.10:${mountPoint}";
     fsType = "nfs";
     options = ["x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "soft" "intr"];
-  };
-
-  fileSystems."/mnt/disks/TRANSFER" = {
-    device = "10.0.0.10:/mnt/disks/TRANSFER";
-    fsType = "nfs";
-    options = ["x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "soft" "intr"];
-  };
+  });
 
   systemd.tmpfiles.rules = [
     "d /mnt/user 0755 root root -"
