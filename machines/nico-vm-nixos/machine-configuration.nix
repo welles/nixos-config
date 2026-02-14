f2b12a818919148c09b9bd1e285b9791d12ba82f
Nico Welles <nico@welles.email>
2026-02-14 12:50:25 +0100

    Add example NFS share to emulation Unraid share

--- machines/nico-vm-nixos/machine-configuration.nix
+++ machines/nico-vm-nixos/machine-configuration.nix
@@ -5,4 +5,13 @@
   modulesPath,
   ...
 }: {
+  fileSystems."/mnt/user/emulation" = {
+    device = "10.0.0.10:/mnt/user/emulation";
+    fsType = "nfs";
+    options = ["x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "nfsvers=4.2"];
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /mnt/user 0755 root root -"
+  ];
 }
