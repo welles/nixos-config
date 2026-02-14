1069202c7236ccc61c80e7be6ab22e60e378cd44
Nico Welles <nico@welles.email>
2026-02-14 13:20:01 +0100

    Negotiate best NFS version for VM mounts

--- machines/nico-vm-nixos/machine-configuration.nix
+++ machines/nico-vm-nixos/machine-configuration.nix
@@ -8,13 +8,13 @@
   fileSystems."/mnt/user/emulation" = {
     device = "10.0.0.10:/mnt/user/emulation";
     fsType = "nfs";
-    options = ["x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "nfsvers=4.2"];
+    options = ["x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "soft" "intr"];
   };
 
   fileSystems."/mnt/disks/TRANSFER" = {
     device = "10.0.0.10:/mnt/disks/TRANSFER";
     fsType = "nfs";
-    options = ["x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "nfsvers=4.2"];
+    options = ["x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "soft" "intr"];
   };
 
   systemd.tmpfiles.rules = [
