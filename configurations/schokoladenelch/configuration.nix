418bbc5608db3d33c09b8caca5373e430ff9339f
Nico Welles <nico@welles.email>
2026-02-21 22:19:49 +0100

    Allow ports 80 and 443 through firewall

--- configurations/schokoladenelch/configuration.nix
+++ configurations/schokoladenelch/configuration.nix
@@ -6,6 +6,9 @@
 }: {
   networking.networkmanager.enable = true;
 
+  networking.firewall.allowedTCPPorts = [80 443];
+  networking.firewall.allowedUDPPorts = [443];
+
   environment.systemPackages = with pkgs; [
     docker-compose
     lazydocker
