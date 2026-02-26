0df75c330fcf373adf6bc3db5b75d49abcf831ba
Nico Welles <nico@welles.email>
2026-02-26 16:39:09 +0100

    Add just MACs for Cloudflare connection

--- configurations/schokoladenelch/configuration.nix
+++ configurations/schokoladenelch/configuration.nix
@@ -188,6 +188,13 @@
       PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
       PermitRootLogin = "no";
+      Macs = [
+        "hmac-sha2-512-etm@openssh.com"
+        "hmac-sha2-256-etm@openssh.com"
+        "umac-128-etm@openssh.com"
+        "hmac-sha2-512" # Cloudflare required
+        "hmac-sha2-256" # Cloudflare required
+      ];
     };
   };
 
