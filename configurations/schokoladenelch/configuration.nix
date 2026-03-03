e20035336ecba20fdefc90165432d731a92f8f22
Nico Welles <nico@welles.email>
2026-03-03 18:16:01 +0100

    Allow unfree netdata web UI

--- configurations/schokoladenelch/configuration.nix
+++ configurations/schokoladenelch/configuration.nix
@@ -195,6 +195,9 @@
 
   services.netdata = {
     enable = true;
+    package = pkgs.netdata.override {
+      withCloudUi = true;
+    };
     configDir = {
       "health_alarm_notify.conf" = pkgs.writeText "health_alarm_notify.conf" ''
         SEND_EMAIL="YES"
