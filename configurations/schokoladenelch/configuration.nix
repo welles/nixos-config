cc00f1403a085155eaf9810664ab369c70371dff
Nico Welles <nico@welles.email>
2026-03-11 17:50:15 +0100

    Moved machine configuration of server into main configuration

--- configurations/schokoladenelch/configuration.nix
+++ configurations/schokoladenelch/configuration.nix
@@ -1,12 +1,46 @@
 {
   config,
   pkgs,
+  lib,
   user,
   ...
 }: {
   imports = [
     ../../stacks/default.nix
   ];
+
+  networking.hostId = "c0ffee12";
+
+  fileSystems."/mnt/bucket/persist".neededForBoot = true;
+  fileSystems."/mnt/bucket/apps".neededForBoot = true;
+
+  boot.initrd.supportedFilesystems = ["zfs"];
+  boot.initrd.postDeviceCommands = lib.mkAfter ''
+    udevadm settle
+    zpool import -d /dev/disk/by-id -N -f bucket || true
+    zfs rollback -r bucket/root@blank && echo -e "\e[32m  rollbacked to blank root\e[0m" || echo -e "\e[31m  no blank root snapshot found, skipping rollback\e[0m"
+  '';
+
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = ["zfs"];
+  boot.zfs.forceImportAll = true;
+  boot.zfs.devNodes = "/dev/disk/by-id";
+  boot.loader.grub = {
+    enable = true;
+    zfsSupport = true;
+    efiSupport = true;
+    mirroredBoots = [
+      {
+        path = "/boot";
+        devices = ["nodev"];
+      }
+      {
+        path = "/boot-fallback";
+        devices = ["nodev"];
+      }
+    ];
+  };
 
   networking.networkmanager.enable = true;
 
