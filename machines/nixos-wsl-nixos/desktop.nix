6dd029a0dd79fa54a749544f29b18ac3d8ac3180
Nico Welles <nico@welles.email>
2026-04-21 10:54:27 +0200

    Remove commented out code

--- machines/nixos-wsl-nixos/desktop.nix
+++ machines/nixos-wsl-nixos/desktop.nix
@@ -1,6 +1,5 @@
 {
   pkgs,
-  lib,
   ...
 }: {
   services = {
@@ -13,12 +12,4 @@
       openFirewall = true;
     };
   };
-
-  # Override xrdp-sesman PAM: the default `login` chain pulls in modules (pam_loginuid etc.) that fail under the WSL kernel
-  # security.pam.services.xrdp-sesman.text = lib.mkForce ''
-  #   auth     required    pam_unix.so nodelay
-  #   account  required    pam_unix.so
-  #   password required    pam_unix.so
-  #   session  required    pam_unix.so
-  # '';
 }
