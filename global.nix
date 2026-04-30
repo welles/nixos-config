71830f0291b72263614a76ee76966450844fa9bc
Nico Welles <nico@welles.email>
2026-04-30 20:03:09 +0200

    Fix determinate nix subsituters url

--- global.nix
+++ global.nix
@@ -31,7 +31,7 @@
   nix = {
     settings = {
       auto-optimise-store = true;
-      extra-substituters = ["https://install.determinate.systems"];
+      extra-substituters = ["https://cache.flakehub.com"];
       extra-trusted-public-keys = ["cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="];
     };
     gc = {
