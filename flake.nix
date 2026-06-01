3484ee56b8f4769800f237bec4557e25678de88b
Nico Welles <nico@welles.email>
2026-06-01 08:24:12 +0000

    Update flake inputs to new stable version 26.05

--- flake.nix
+++ flake.nix
@@ -15,10 +15,10 @@
 #   - nixos-virtualbox-nixos:     VirtualBox VM development environment
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager-unstable = {
