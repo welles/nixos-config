55d2a390169b422f1d6abf5f81144a1381752109
Nico Welles <nico@welles.email>
2026-06-11 23:37:09 +0200

    Remove unnecessary sops and i18n settings

--- configurations/nico/default.nix
+++ configurations/nico/default.nix
@@ -22,30 +22,9 @@
     ./rclone.nix
   ];
 
-  # Base sops-nix configuration: use a shared age key for all nico machines.
-  # Machine-specific secrets use defaultSopsFile in the machine config;
-  # nico-level secrets (e.g. rclone) specify their sopsFile explicitly.
-  sops = {
-    age.keyFile = "/var/lib/sops-nix/key.txt";
-    defaultSopsFormat = "yaml";
-    validateSopsFiles = true;
-  };
+  sops.age.keyFile = "/var/lib/sops-nix/key.txt";
 
-  # Disable waiting for NetworkManager at boot to speed up startup
   systemd.services.NetworkManager-wait-online.enable = false;
-
-  # German locale settings for all LC_* categories
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
-  };
 
   # User account
   users.users.${user} = {
