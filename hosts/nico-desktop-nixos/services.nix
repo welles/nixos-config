29356387235afff8aaf3543a47c4690d5dd9505d
Nico Welles <nico@welles.email>
2026-06-29 20:21:41 +0200

    Enable autologin on desktop

--- hosts/nico-desktop-nixos/services.nix
+++ hosts/nico-desktop-nixos/services.nix
@@ -1,6 +1,11 @@
 {...}: {
   imports = [../../modules/removable-media.nix];
 
+  services.displayManager.autoLogin = {
+    enable = true;
+    user = "nico";
+  };
+
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
