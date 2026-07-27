7c6afa5cfb72cfba95607bf4901cc70783efa3cd
Nico Welles <nico@welles.email>
2026-07-28 01:59:39 +0200

    Make Nix GC way more aggressive

--- modules/nix-settings.nix
+++ modules/nix-settings.nix
@@ -13,8 +13,8 @@ _: {
     };
     gc = {
       automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
+      dates = "daily";
+      options = "--delete-older-than 7d";
     };
   };
 }
