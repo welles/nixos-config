0a7a421982efec01aff2381a7fadf25d4de5f67f
Nico Welles <nico@welles.email>
2026-09-11 08:29:13 +0000

    Add extensions to hg

--- modules/packages/hg.nix
+++ modules/packages/hg.nix
@@ -1,11 +1,21 @@
 {
   userDescription ? (builtins.throw "hg.nix: set `_module.args.userDescription` to the user display name"),
   userEmail ? (builtins.throw "hg.nix: set `_module.args.userEmail` to the user email"),
+  pkgs,
   ...
 }: {
   programs.mercurial = {
     enable = true;
+    package = pkgs.mercurial.withExtensions (pythonPackages: [
+      pythonPackages.hg-evolve
+    ]);
     userName = userDescription;
-    userEmail = userEmail;
+    inherit userEmail;
+    extraConfig.extensions = {
+      evolve = "";
+      histedit = "";
+      rebase = "";
+      topic = "";
+    };
   };
 }
