2e468beeb33eeeac748d25581b18b0b0dac27cf0
Nico Welles <nico@welles.email>
2026-09-14 12:25:09 +0000

    Add Cachix as cache for flake

--- modules/nix-settings.nix
+++ modules/nix-settings.nix
@@ -10,6 +10,8 @@ _: {
     settings = {
       auto-optimise-store = true;
       experimental-features = ["nix-command" "flakes"];
+      substituters = ["https://welles-nixos-config.cachix.org"];
+      trusted-public-keys = ["welles-nixos-config.cachix.org-1:vCSK0/NERCDRPEsW6yCjweDx4Xw7O11j7nEUkwvUA5o="];
     };
     gc = {
       automatic = true;
