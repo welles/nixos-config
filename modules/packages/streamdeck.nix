58e452ab9f8d19cfcd042260efc76fd4d2f98f6c
Nico Welles <nico@welles.email>
2026-09-23 23:40:32 +0200

    Add autostart entry to StreamController module

--- modules/packages/streamdeck.nix
+++ modules/packages/streamdeck.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  pkgs,
   user,
   persistRoot ? null,
   ...
@@ -9,4 +10,11 @@
   environment.persistence = lib.mkIf (persistRoot != null) {
     ${persistRoot}.users.${user}.directories = [".var/app/com.core447.StreamController"];
   };
+
+  home-manager.users.${user}.xdg.configFile."autostart/StreamController.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Name=StreamController
+    Exec=${pkgs.streamcontroller}/bin/streamcontroller -b
+  '';
 }
